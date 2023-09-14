73e083c317247636ab33d5b1662b2b969cf4c7c6
Janoviic <janoviccc.filip@gmail.com>
2023-09-14 18:23:25 +0200

    Initial commit

--- src/paket/Prvaklasa.java
+++ src/paket/Prvaklasa.java
@@ -4,9 +4,15 @@ public class Prvaklasa {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * Komentar
-		 */
+		int []a= {1,2,3,4,5};
+		for(int i = 0; i < a.length; i++) {
+			if((a[i]%2)==0) {
+				System.out.println("paran"+ a[i]);
+			}else {
+				System.out.println("neparan"+ a[i]);
+			}
+			
+		}
 	}
 
 }
